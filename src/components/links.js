2164a463817948cb1c5e1f6c38bd661e301e7faf
ybadr99 <yousefbadr909@gmail.com>
2023-06-19 16:21:36 +0300

    add dragons directory

--- src/components/links.js
+++ src/components/links.js
@@ -1,6 +1,7 @@
 const links = [
   { path: '/', text: 'Rockets' },
-  { path: 'categories', text: 'Missions' },
+  { path: 'dragons', text: 'Dragons' },
+  { path: 'missions', text: 'Missions' },
   { path: 'myprofile', text: 'My Profile' },
 ];
 export default links;
