db35d329a33c5d33d612bf4c09781555107947b6
ybadr99 <yousefbadr909@gmail.com>
2023-06-19 16:16:14 +0300

    style logo item and change assets directory

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,22 @@ import { NavLink } from 'react-router-dom';
 import links from './links';
 
 const Navbar = () => (
-  <div>
-    <ul className="navbar">
-      {links.map((link) => (
-        <li className="listitems" key={link.text}>
-          <NavLink to={link.path}>{link.text}</NavLink>
-        </li>
-      ))}
-    </ul>
-  </div>
+  <>
+    <header>
+      <div className="logo">
+        <img src="./assets/planet.png" alt="" />
+        <span> Space Travelers Hub</span>
+      </div>
+      <ul className="navbar">
+        {links.map((link) => (
+          <li className="listitems" key={link.text}>
+            <NavLink to={link.path}>{link.text}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </header>
+
+    {/* <hr /> */}
+  </>
 );
 export default Navbar;
