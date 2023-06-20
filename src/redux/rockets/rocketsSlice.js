361c460703a422967d313a36fca7c329fcf031ba
vangelif <ev.fotiades@yahoo.com>
2023-06-20 10:00:12 +0300

    fetch data from api

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,43 +1,48 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-// const api = 'https://api.spacexdata.com/v4/rockets';
+const api = 'https://api.spacexdata.com/v4/rockets';
 
-// export const getRockets = createAsyncThunk('rockets/getRockets', async () =>
-//   fetch(api)
+export const getRockets = createAsyncThunk('rockets/getRockets', async () => {
+  const resp = await fetch(api);
+  if (!resp.ok) {
+    throw new Error('rockets fetched unsuccessfully');
+  }
+  const data = await resp.json();
+  return data;
+});
 
-//     .then((resp) => resp.json())
-//     .catch((err) => console.log(error))
-// console.log(resp.json());
-//     );
-
-const initialState = {
-  id: '123s',
-  rocket_name: 'sad',
-  description: 'asd',
-  flickr_images: 'https://imgur.com/DaCfMsj.jpg',
-  isLoading: false,
-  error: null,
-  status: 'idle',
-};
+// const initialState = {
+//   id: '123s',
+//   rocket_name: 'sad',
+//   description: 'asd',
+//   flickr_images: 'https://imgur.com/DaCfMsj.jpg',
+//   isLoading: false,
+//   error: null,
+//   status: 'idle',
+// };
 
 const rocketSlice = createSlice({
   name: 'rockets',
-  initialState,
-  //   extraReducers: {
-  //     [getRockets.pending]: (state) => {
-  //       // console.log(action);
-  //       state.isLoading = false;
-  //     },
-  //     [getRockets.fullfiled]: (state, action) => {
-  //       console.log(action);
-  //       state.isLoading = false;
-  //       state.rockets = action.payload;
-  //     },
-  //     [getRockets.rejected]: (state) => {
-  //       // console.log(action);
-  //       state.isLoading = false;
-  //     },
-  //   },
+  initialState: {
+    rockets: [],
+    error: null,
+    status: 'idle',
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getRockets.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getRockets.fulfilled, (state, action) => {
+        state.status = 'success';
+        state.rockets = action.payload;
+      })
+      .addCase(getRockets.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default rocketSlice.reducer;
