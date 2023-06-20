402f6aa1b2d289fe7d7a643695dba76ccab4fba3
vangelif <ev.fotiades@yahoo.com>
2023-06-20 14:24:32 +0300

    add stylings for the reserve button effect

---
+++ src/components/Dragons.js
@@ -0,0 +1,10 @@
+import '../styles/dragons.css';
+
+const Dragons = () => (
+  <>
+    <div className="dragon">hello</div>
+    <div className="world">world</div>
+    <div />
+  </>
+);
+export default Dragons;
