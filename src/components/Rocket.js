7861524d3f07bd35f6833139c2a56a03adccf718
vangelif <ev.fotiades@yahoo.com>
2023-06-20 10:01:33 +0300

    scaffold api data to Rocket component

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,26 +1,44 @@
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRockets } from '../redux/rockets/rocketsSlice';
 
 const Rocket = () => {
-  const rockets = useSelector((state) => state.rockets);
-  console.log(rockets);
-  // eslint-disable-next-line react/jsx-indent
-  <section>
-    {rockets.map((rocket) => (
-      <div key={rocket.id}>
-        <div>
-          <img src={rocket.flickr_images} alt="123" />
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets.rockets);
+  const status = useSelector((state) => state.rockets.status);
+  const error = useSelector((state) => state.rockets.error);
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(getRockets());
+    }
+  }, [status, dispatch]);
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+  if (status === 'failed') {
+    return <div>{error}</div>;
+  }
+  return (
+    <section>
+      {rockets.map((rocket) => (
+        <div key={rocket.id}>
+          <div>
+            <img src={rocket.flickr_images} alt="123" />
+          </div>
+          <div>
+            <h3>{rocket.name}</h3>
+            <p>
+              <span>Reserved</span>
+              {rocket.description}
+            </p>
+            <button type="button">Reserve Rocket</button>
+            <button type="button">Cancel Reservation</button>
+          </div>
         </div>
-        <div>
-          <h3>Rocket Name</h3>
-          <p>
-            <span>Reserved</span>
-            Description
-          </p>
-          <button type="button">Reserve Rocket</button>
-          <button type="button">Cancel RSVP</button>
-        </div>
-      </div>
-    ))}
-  </section>;
+      ))}
+    </section>
+  );
 };
 export default Rocket;
