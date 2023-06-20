72c3a31f3420715b73ac70b9340eb5b0245af98e
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-20 17:06:35 +0600

    Add mission Slice

---
+++ src/redux/missions/missionsSlice.js
@@ -0,0 +1,47 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const api = 'https://api.spacexdata.com/v3/missions';
+
+export const fetchMissions = createAsyncThunk('missions/fetcMissions', async () => {
+  const resp = await fetch(api);
+  if (!resp.ok) {
+    throw new Error('rockets fetched unsuccessfully');
+  }
+  const data = await resp.json();
+  return data;
+});
+
+// const initialState = {
+//   mission_id: '123s',
+//   mission_name: 'sad',
+//   description: 'asd',
+//   isLoading: false,
+//   error: null,
+//   status: 'idle',
+// };
+
+const missionSlice = createSlice({
+  name: 'missions',
+  initialState: {
+    missions: [],
+    error: null,
+    status: 'idle',
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMissions.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchMissions.fulfilled, (state, action) => {
+        state.status = 'success';
+        state.rockets = action.payload;
+      })
+      .addCase(fetchMissions.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default missionSlice.reducer;
