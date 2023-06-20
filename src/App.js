811d360d8c811d6c734dec44de74bafb9ed37b2c
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-20 17:08:06 +0600

    Add missions to the links

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Rocket from './components/Rocket';
+import Mission from './components/Missions';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route className="navlink" path="/" element={<Rocket />} />
+        <Route className="navlink" path="missions" element={<Mission />} />
         <Route className="navlink" path="categories" />
         <Route className="navlink" path="myprofile" />
       </Routes>
