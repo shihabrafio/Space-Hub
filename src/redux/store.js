20155d3e8305a6141a9053dcbf5436fea8e9411d
vangelif <ev.fotiades@yahoo.com>
2023-06-20 10:00:50 +0300

    build store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import rocketsSlice from './rockets/rocketsSlice';
+import rocketReducer from './rockets/rocketsSlice';
 
 const store = configureStore({
   reducer: {
-    rockets: rocketsSlice,
+    rockets: rocketReducer,
   },
 });
 export default store;
