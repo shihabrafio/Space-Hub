074e4381f4dddefac708b43e9b39cf7c332aa547
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-21 20:02:59 +0600

    Fix Linter Error

--- src/__test__/Missions.test.js
+++ src/__test__/Missions.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { render as rtlRender } from '@testing-library/react';
-import Missions from '../components/Missions';
 import { Provider } from 'react-redux';
+import Missions from '../components/Missions';
 import store from '../redux/store';
 
 const render = (component) => rtlRender(
-    <Provider store={store}>
-      {component}
-    </Provider>,
-  );
+  <Provider store={store}>
+    {component}
+  </Provider>,
+);
 
 describe('MyRocketss', () => {
   it('render Mission Components', () => {
--- src/__test__/Mymission.test.js
+++ src/__test__/Mymission.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { render as rtlRender } from '@testing-library/react';
-import Mymissions from '../__mocks__/Mymission';
 import { Provider } from 'react-redux';
+import Mymissions from '../__mocks__/Mymission';
 import store from '../redux/store';
 
 const render = (component) => rtlRender(
-    <Provider store={store}>
-      {component}
-    </Provider>,
-  );
-  
+  <Provider store={store}>
+    {component}
+  </Provider>,
+);
+
 describe('MyMissions', () => {
   it('render My Missions Components', () => {
     render(<Mymissions />);
