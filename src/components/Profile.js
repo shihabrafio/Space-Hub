ba14892ca367a041fc7a8dcd1c36b70ea7538e32
vangelif <ev.fotiades@yahoo.com>
2023-06-21 14:47:07 +0300

    scaffold rockets in my profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,40 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
 
 const Profile = () => {
   const { dragons } = useSelector((state) => state.dragons);
   const filteredDragons = dragons.filter((dragon) => dragon.reserved);
 
+  const { rockets } = useSelector((state) => state.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+
   return (
-    <div>
-      {filteredDragons.map((dragon) => (
-        <h3 key={dragon.id}>{dragon.name}</h3>
-      ))}
-    </div>
+    <section className="profile-container">
+      <div className="dragons-wrapper">
+        <h2>My Dragons</h2>
+        <table>
+          <tbody>
+            {filteredDragons.map((dragon) => (
+              <tr key={dragon.id}>
+                <td>{dragon.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="rockets-wrapper">
+        <h2>My Rockets</h2>
+        <table>
+          <tbody>
+            {reservedRockets.map((rocket) => (
+              <tr key={rocket.id}>
+                <td>{rocket.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
   );
 };
 
