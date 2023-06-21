720169758736a793625a1f85b747c6687ea85f2b
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-21 15:17:54 +0600

    Add Mymissions

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,19 @@
 import { useSelector } from 'react-redux';
+import MyMissions from './Mymission';
 
 const Profile = () => {
   const { dragons } = useSelector((state) => state.dragons);
   const filteredDragons = dragons.filter((dragon) => dragon.reserved);
 
   return (
-    <div>
-      {filteredDragons.map((dragon) => (
-        <h3 key={dragon.id}>{dragon.name}</h3>
-      ))}
-    </div>
+    <>
+      <div>
+        {filteredDragons.map((dragon) => (
+          <h3 key={dragon.id}>{dragon.name}</h3>
+        ))}
+      </div>
+      <MyMissions />
+    </>
   );
 };
 
